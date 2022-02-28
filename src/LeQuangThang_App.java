7cea2bafa29ce2830619a901de1c920f58ea9dcc
Le-Quang-Thang <97603954+Le-Quang-Thang@users.noreply.github.com>
2022-02-28 10:55:15 +0700

    first commit

---
+++ src/LeQuangThang_App.java
@@ -0,0 +1,8 @@
+
+public class LeQuangThang_App
+{
+	public static void main(String[] args) 
+	   {
+		   System.out.println("Le Quang Thang Thang hoc Java!");
+	   }
+}
---
+++ src/module-info.java
@@ -0,0 +1,2 @@
+module lequangthang_App.java {
+}
