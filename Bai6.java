6c0f7be84948e4d6a3e160c9b54f5c4ffff1a38c
Lê Quang Thắng <97603954+Le-Quang-Thang@users.noreply.github.com>
2022-03-16 10:53:17 +0700

    Create Bai6.java

---
+++ Bai6.java
@@ -0,0 +1,16 @@
+package helloworld;
+import java.util.Scanner;
+public class bai6 {
+    public static void main(String[]args) { 
+        Scanner sc = new Scanner(System.in);
+        System.out.print("nhap vao n: " );
+        int n = sc.nextInt();
+       int tong=1;
+       int i=1;
+       while ( i <= n) {
+		tong *= i;      
+		++i;
+	}
+        System.out.print("giai thua cua n: " + tong);
+}
+}
